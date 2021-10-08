2513c65a9925ba572abac9460ca4d347e69303c1
flamechain <rwc@thezulus.com>
2021-10-07 23:04:38 -0700

    started NSLib rewrite (look at trello board 'NSLib Rewrite' for more info)

--- ECA/libexec/nslib/test/test.cpp
+++ ECA/libexec/nslib/test/test.cpp
@@ -1,9 +1,25 @@
-#include "nsstring"
+#include <nsstring_new>
+#include <stdio.h>
+
+string testPrints() {
+    string test1 = "Hello, world!";
+    string test2 = (string)'H';
+    string test3 = test1;
+
+    printf("test1: %s\ntest2: %s\ntest3: %s\n", test1.c_str(), test2.c_str(), test3.c_str());
+
+    return test3;
+}
 
 int main() {
-    string test = "asdfHelloWorldasdf";
+    string test = testPrints();
 
-    printf("%s\n", test.substring(4).c_str());
+    printf("test: %s\n", test.c_str());
+
+    string test2 = "Hello, world2?";
+
+    test = test2;
+    printf("test: %s\n", test.c_str());
 
     return 0;
 }
