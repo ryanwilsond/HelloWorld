2b074db37758d4af8818b2590d63de4b39ce558b
FlameChain <rwc@thezulus.com>
2021-11-16 11:42:08 -0800

    progress on bug with reference

--- ECA/libexec/nslib/tests/test.cpp
+++ ECA/libexec/nslib/tests/test.cpp
@@ -1,13 +1,37 @@
+// #define _TESTING
+
+#ifdef _TESTING
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+#else
 #include "nsstring"
 #include "nsvector"
+#endif
 
 #include <stdio.h>
 
 int main() {
+    // vector<string> test;
+    // test.assign(0, string("Hello, world!"));
+        // cant do this because [] operator returns a value, not a pointer
+        // test[0] = string("Hello, world!");
+    // printf("strres: %i, strsize: %i, strval: %s\n", (int)test[0]._Myres(), (int)test[0]._Mysize(), test[0].c_str());
+
+    // vector<int> test;
+    // test.assign(0, 10);
+    // test.append(11);
+    // printf("value: %i, value: %i\n", test[0], test[1]);
+
     vector<string> test;
-    test.assign(0, string("Hello, world!"));
-    // test[0] = string("Hello, world!");
-    printf("strres: %i, strsize: %i, strval: %s\n", (int)test[0]._Myres(), (int)test[0]._Mysize(), test[0].c_str());
+    unsigned int testit = 0;
+    // vector<string>::iterator testit;
+    // testit = test.begin();
+    test.assign(testit, string("Hello, world!"));
+    test.append(string("another string"));
+    printf("value: %s, value: %s\n", test[0].c_str(), test[1].c_str());
 
     return 0;
 }
