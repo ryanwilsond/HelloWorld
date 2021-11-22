5ccde803b8147aad9b32016c690af74288e277ae
flamechain <rwc@thezulus.com>
2021-11-22 14:39:09 -0800

    finished ordered & unordered map implementation

--- ECA/libexec/nslib/tests/src/nsmap_test.cpp
+++ ECA/libexec/nslib/tests/src/nsmap_test.cpp
@@ -5,7 +5,9 @@
 #include <stdio.h>
 
 int main() {
-    map<char, int> mymap = {
+    printf("unordered\n");
+
+    unordered_map<char, int> mymap = {
         make_pair('a', 1),
         make_pair('b', 2),
         make_pair('c', 3),
@@ -14,8 +16,28 @@ int main() {
     };
 
     mymap.insert('e', 5);
+    mymap.insert('f', 7);
+    mymap.insert('j', 2);
 
     for (pair<char, int> p : mymap) {
+        printf("%c:%i, ", p.key(), p.value());
+    } printf("\n");
+
+    printf("ordered\n");
+
+    map<char, int> mymap2 = {
+        make_pair('a', 1),
+        make_pair('b', 2),
+        make_pair('c', 3),
+        make_pair('d', 4),
+        make_pair('f', 6),
+    };
+
+    mymap2.insert('e', 5);
+    mymap2.insert('f', 7);
+    mymap2.insert('j', 2);
+
+    for (pair<char, int> p : mymap2) {
         printf("%c:%i, ", p.key(), p.value());
     } printf("\n");
 
--- ECA/libexec/nslib/tests/test.cpp
+++ ECA/libexec/nslib/tests/test.cpp
@@ -1,38 +1,26 @@
 /* less formal test file */
-// #define _TESTING
-
-#ifdef _TESTING
-#include <string>
-#include <vector>
-
-using std::string;
-using std::vector;
-#else
-#include "nsstring"
-#include "nsvector"
-#endif
 
 #include <stdio.h>
 
+
+    inline long long ceil(long double _Float)
+    {
+    long long _Floor = (long long)_Float;
+
+    if (_Floor == _Float)
+        {
+        return _Floor;
+        }
+
+    return _Floor + 1;
+    }
+
 int main() {
-    // vector<string> test;
-    // test.assign(0, string("Hello, world!"));
-        // cant do this because [] operator returns a value, not a pointer
-        // test[0] = string("Hello, world!");
-    // printf("strres: %i, strsize: %i, strval: %s\n", (int)test[0]._Myres(), (int)test[0]._Mysize(), test[0].c_str());
+    float num = 3.0;
+    float num2 = 3.1;
+    float num3 = 3.9;
 
-    // vector<int> test;
-    // test.assign(0, 10);
-    // test.append(11);
-    // printf("value: %i, value: %i\n", test[0], test[1]);
-
-    vector<string> test;
-    unsigned int testit = 0;
-    // vector<string>::iterator testit;
-    // testit = test.begin();
-    test.assign(testit, string("Hello, world!"));
-    test.append(string("another string"));
-    printf("value: %s, value: %s\n", test[0].c_str(), test[1].c_str());
+    printf("%i, %i, %i\n", ceil(num), ceil(num2), ceil(num3));
 
     return 0;
 }
